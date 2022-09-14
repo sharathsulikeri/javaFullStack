8b347c07e096fd44b9065039fb13eb5a4a039f83
SD <sharathsulikeri8@gmail.com>
2022-09-14 22:57:26 +0530

    added fooditem

---
+++ FoodItem.java
@@ -0,0 +1,38 @@
+class FoodItem{
+	 
+	  public static void main(String[] yolo)
+	  {
+		  // foods item
+		  String food1="idli";
+		  String food2="dose";
+		  String food3="panipuri";
+		  String food4="bhel";
+		  String foos5="chikken";
+		  String food6="gobi";
+		  String food7="vadaphav";
+		  String food8="lolipop";
+		  String food9="chips";
+		  String food10="kajukurma";
+		  String food11="biscuit";
+		  String food12="cake";
+		  String food13="kabab";
+		  
+       
+          String[] fooditems={"idli","food2","food3","food4","food5","food6","food7","food8","food9","food10","food11","food12",};
+		  int count=fooditems.length;
+		  
+		  System.out.println("count of food item :"+ count);
+		   
+                 fooditem[2]="burger";
+		  
+		  
+		  for(int foodiee=0; foodiee<=fooditems.length; foodiee++)
+		  {
+			  System.out.println("total food items :"+ foodiee);
+			  
+			  index[] ref(3)="burger";
+			 
+			  
+		  }
+	  }
+}
