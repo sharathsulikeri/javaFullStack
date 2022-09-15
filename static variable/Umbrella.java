f64c76b59e3cb6664515603d9bd8fbee13bc6b5c
SD <sharathsulikeri8@gmail.com>
2022-09-16 00:38:15 +0530

    Basic programms

---
+++ static variable/Umbrella.java
@@ -0,0 +1,30 @@
+class Umbrella{
+	
+	static void safewalking()
+	{
+		System.out.println("safewalking when its rain");
+	}
+    static void open()
+	{
+         System.out.println("umbrella is a open");
+		 button();
+	}
+	static void close()
+	{
+		System.out.println("umbrella is a close");
+	}
+    static void button()
+	{
+		System.out.println("button to close");
+	}
+	static void rain()
+	{
+		System.out.println("it is using raining time");
+	}
+	static void photography()
+	{
+		System.out.println("it is using photoshoot");
+
+	}
+	
+}
---
+++ static variable/UmbrellaRunner.java
@@ -0,0 +1,15 @@
+class UmbrellaRunner{
+	
+	public static void main(String[] args){
+		
+		System.out.println("it helps umbrella when its rain");
+	
+	Umbrella.safewalking();
+	Umbrella.open();
+	Umbrella.close();
+	Umbrella.button();
+    Umbrella.rain();
+	Umbrella.photography();
+	
+	}
+}
